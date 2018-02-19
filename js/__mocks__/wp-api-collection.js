c799433323ba205473220eec2bd250cb732f37a8
Matthew Haines-Young <matthew@matth.eu>
2018-02-19 14:33:07 +0000

    Split up mock

---
+++ js/__mocks__/wp-api-collection.js
@@ -0,0 +1,18 @@
+class MockCollection {
+	fetch() {
+		return new Promise( ( resolve, reject ) => {
+			process.nextTick( () => resolve( [
+				{
+					id:   1,
+					name: 'sport',
+				},
+			] ) );
+		} );
+	}
+
+	hasMore() {
+		return true;
+	}
+}
+
+export default MockCollection;
--- js/__mocks__/wp.js
+++ js/__mocks__/wp.js
@@ -1,21 +1,5 @@
 import React from 'react';
-
-class MockCollection {
-	fetch() {
-		return new Promise( ( resolve, reject ) => {
-			process.nextTick( () => resolve( [
-				{
-					id:   1,
-					name: 'sport',
-				},
-			] ) );
-		} );
-	}
-
-	hasMore() {
-		return true;
-	}
-}
+import MockCollection from './wp-api-collection';
 
 const wp = {
 	components: { Button: props => <button id={ props.id } className={ props.className }>{ props.children }</button> },
