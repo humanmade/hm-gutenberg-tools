6596f48fce5f6db8de71f4b9a7fddb4a004a4a72
Dzikri Aziz <k@kucrut.org>
2018-03-01 07:57:37 +0800

    PostTypeFilter: Convert to stateless component
    YAS!

--- js/components/post-select/post-type-filter.js
+++ js/components/post-select/post-type-filter.js
@@ -7,35 +7,33 @@ import wp from 'wp';
 const { __ } = wp.i18n;
 const { withAPIData } = wp.components;
 
-class PostTypeFilter extends React.Component {
-	render() {
-		const { label, onChange, types, value } = this.props;
-		const id = _uniqueId( 'post-select-post-type-filter' );
+const PostTypeFilter = props => {
+	const { label, onChange, types, value } = props;
+	const id = _uniqueId( 'post-select-post-type-filter' );
 
-		let selectProps = {
-			id,
-			value,
-			backspaceRemoves: false,
-			isLoading:        types.isLoading,
-			multi:            false,
-			onChange:         selected => onChange( selected ),
-		};
+	let selectProps = {
+		id,
+		value,
+		backspaceRemoves: false,
+		isLoading:        types.isLoading,
+		multi:            false,
+		onChange:         selected => onChange( selected ),
+	};
 
-		if ( types.data ) {
-			selectProps = Object.assign( {}, selectProps, {
-				options: Object.keys( types.data ).map( type => ( {
-					label:      types.data[ type ].name,
-					taxonomies: types.data[ type ].taxonomies,
-					value:      types.data[ type ].slug,
-				} ) ),
-			} );
-		}
-
-		return <div className="post-select-filters-row">
-			<label htmlFor={ id }>{ __( 'Type' ) }</label>
-			<Select { ...selectProps } />
-		</div>;
+	if ( types.data ) {
+		selectProps = Object.assign( {}, selectProps, {
+			options: Object.keys( types.data ).map( type => ( {
+				label:      types.data[ type ].name,
+				taxonomies: types.data[ type ].taxonomies,
+				value:      types.data[ type ].slug,
+			} ) ),
+		} );
 	}
+
+	return <div className="post-select-filters-row">
+		<label htmlFor={ id }>{ __( 'Type' ) }</label>
+		<Select { ...selectProps } />
+	</div>;
 }
 
 PostTypeFilter.defaultProps = { value: 'post' };
