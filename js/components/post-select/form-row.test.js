930f0c3cbc9ed3e13ade01f083ebf5e0bfaa3d95
Matthew Haines-Young <matthew@matth.eu>
2018-09-05 12:12:25 +0100

    Get Jest tests working

--- js/components/post-select/form-row.test.js
+++ js/components/post-select/form-row.test.js
@@ -1,22 +1,15 @@
-// jest.mock( 'wp', () => {} );
-import * as React from 'react';
-// import TestRenderer from 'react-test-renderer';
-import { shallow } from 'enzyme';
+import React from 'react';
+import TestRenderer from 'react-test-renderer';
 
 import FormRow from './form-row';
 
 test( 'Form Row', () => {
-// 	const testRenderer = TestRenderer.create(
-// 		React.createElement( FormRow, {
-// 			label: 'Foo',
-// 			labelFor: 'bar',
-// 		}, 'test' )
-// 	);
+	const testRenderer = TestRenderer.create(
+		React.createElement( FormRow, {
+			label: 'Foo',
+			labelFor: 'bar',
+		}, 'test' )
+	);
 
-// 	console.log( testRenderer.toJSON() );
-// 	expect( testRenderer.toJSON() ).toMatchSnapshot();
-
-	const formRow = shallow( <FormRow label="Foo" labelFor="bar">Test</FormRow> );
-	console.log( formRow )
-	// expect( FormRow.children( '.post-list-item' ).length ).toBe( 0 );
+	expect( testRenderer.toJSON() ).toMatchSnapshot();
 } );
