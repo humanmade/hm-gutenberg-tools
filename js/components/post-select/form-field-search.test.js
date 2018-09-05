6901943321c4e291b0b5772d9456704fb78d072d
Matthew Haines-Young <matthew@matth.eu>
2018-09-05 12:28:50 +0100

    Search field test

---
+++ js/components/post-select/form-field-search.test.js
@@ -0,0 +1,19 @@
+// import jest from 'jest';
+import React from 'react';
+import TestRenderer from 'react-test-renderer';
+
+import FormFieldSearch from './form-field-search';
+
+test( 'Form Row', () => {
+	const onChangeHandler = jest.fn();
+
+	const testRenderer = TestRenderer.create(
+		<FormFieldSearch fieldId="id-123" label="Search Field" value="test value" onChange={ onChangeHandler } />
+	);
+
+	expect( testRenderer.toJSON() ).toMatchSnapshot();
+
+	testRenderer.root.findByType( 'input' ).props.onChange( { target: { value: 'new value' } } );
+	expect( onChangeHandler.mock.calls.length ).toBe( 1 );
+	expect( onChangeHandler.mock.calls[0][0] ).toBe( 'new value' );
+} );
