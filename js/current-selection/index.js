841a0df4ba2d01d1eac00de52585470cb8e21bbf
Milen Petrinski <gonzo@greatgonzo.net>
2021-11-02 13:47:22 +0200

    Register post select endpoint with wp.data for use with getEntityRecords().

--- js/current-selection/index.js
+++ js/current-selection/index.js
@@ -2,14 +2,26 @@ import wp from 'wp';
 
 import CurrentSelection from './components/current-selection';
 
-const { withSelect } = wp.data;
+const { withSelect, dispatch } = wp.data;
+
+dispatch( 'core' ).addEntities( [
+	{
+		name: 'post-select',
+		kind: 'hm-gb-tools/v1',
+		baseURL: '/hm-gb-tools/v1/post-select',
+	},
+] );
 
 export default withSelect( ( select, ownProps ) => {
 	const { getEntityRecords } = select( 'core' );
 	const { postIds, postType } = ownProps;
-	const posts = getEntityRecords( 'postType', postType, {
+	const postTypes = Array.isArray( postType ) ? postType : [ postType ];
+	const posts = getEntityRecords( 'hm-gb-tools/v1', 'post-select', {
 		include: postIds,
+		per_page: postIds.length,
+		types: postTypes,
 		orderby: 'include',
+		context: 'view',
 	} ) || [];
 
 	return {
