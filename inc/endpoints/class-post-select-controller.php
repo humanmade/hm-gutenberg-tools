752fc020df6b2ee1975c42c229c14a872030e052
Rufaro Madamombe <rufaro@humanmade.com>
2021-12-01 14:38:42 +0200

    Add actions to make it possible to modify the query

--- inc/endpoints/class-post-select-controller.php
+++ inc/endpoints/class-post-select-controller.php
@@ -140,7 +140,12 @@ class Post_Select_Controller extends WP_REST_Controller {
 
 		$query_args = apply_filters( 'hm_gb_tools_post_select_query_args', $query_args );
 
+		do_action( 'hm_gb_tools_post_select_before_query' );
+
 		$query = new WP_Query( $query_args );
+
+		do_action( 'hm_gb_tools_post_select_after_query' );
+
 		$posts = [];
 
 		foreach ( $query->posts as $post ) {
