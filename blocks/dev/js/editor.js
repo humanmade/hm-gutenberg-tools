769c0c199b29475ff68e6ff77618fb8d7e9c5e72
Matthew Haines-Young <matthew@humanmade.com>
2021-12-04 20:11:29 +0000

    Add post select button

--- blocks/dev/js/editor.js
+++ blocks/dev/js/editor.js
@@ -1,17 +1,38 @@
 import React from 'react';
 
+import {
+	useBlockProps,
+	InspectorControls,
+} from '@wordpress/block-editor';
 import { registerBlockType } from '@wordpress/blocks';
+import { __ } from '@wordpress/i18n';
 
-function Edit() {
+const { PostControl } = window.hm.controls;
+
+function Edit(  { attributes, setAttributes }  ) {
 	return (
-		<div>Test</div>
+		<div { ...useBlockProps() }>
+			<InspectorControls key="setting">
+				<PostControl
+					btnText={ __( 'Select Page' ) }
+					label={ __( 'Linked Page.' ) }
+					postSelectProps={ { postType: 'page' } }
+					// value={ attributes.testPostControl }
+					// onChange={ value => console.log( value ) }
+				/>
+			</InspectorControls>
+
+			<p>Test</p>
+		</div>
 	);
 }
 
 registerBlockType( 'hm-gb-tools/dev', {
 	apiVersion: 2,
-	/**
-	 * @see ./edit.js
-	 */
+	attributes: {
+		testPostControl: {
+			type: 'array',
+		},
+	},
 	edit: Edit,
 } );
